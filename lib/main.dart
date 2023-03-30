e535dd1e32d980de2136eff62c74046e59a5a6b4
Markus_Leonard_Wijaya <markus.leonard24@gmail.com>
2023-03-30 06:57:48 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/fitnessapp/generatedframe1widget/GeneratedFrame1Widget.dart';
+import 'package:flutterapp/fitnessapp/generatedframe1widget1/GeneratedFrame1Widget1.dart';
+
+void main() {
+  runApp(fitnessApp());
+}
+
+class fitnessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedFrame1Widget',
+      routes: {
+        '/GeneratedFrame1Widget': (context) => GeneratedFrame1Widget(),
+        '/GeneratedFrame1Widget1': (context) => GeneratedFrame1Widget1(),
+      },
+    );
+  }
+}
