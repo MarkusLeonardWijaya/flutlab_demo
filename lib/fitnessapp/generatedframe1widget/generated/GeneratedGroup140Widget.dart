653895432612eab13d97c34bde65d7a5592b4788
Markus_Leonard_Wijaya <markus.leonard24@gmail.com>
2023-03-31 07:05:21 +0000

    remove overflow again

--- lib/fitnessapp/generatedframe1widget/generated/GeneratedGroup140Widget.dart
+++ lib/fitnessapp/generatedframe1widget/generated/GeneratedGroup140Widget.dart
@@ -10,21 +10,18 @@ class GeneratedGroup140Widget extends StatelessWidget {
     return Container(
       width: 503.3598327636719,
       height: 114.4953384399414,
-      child: Stack(
-          fit: StackFit.expand,
-          alignment: Alignment.center,
-          overflow: Overflow.visible,
-          children: [
-            Positioned(
-              left: 0.0,
-              top: 0.0,
-              right: null,
-              bottom: null,
-              width: 503.3598327636719,
-              height: 114.4953384399414,
-              child: GeneratedClip138Widget(),
-            )
-          ]),
+      child:
+          Stack(fit: StackFit.expand, alignment: Alignment.center, children: [
+        Positioned(
+          left: 0.0,
+          top: 0.0,
+          right: null,
+          bottom: null,
+          width: 503.3598327636719,
+          height: 114.4953384399414,
+          child: GeneratedClip138Widget(),
+        )
+      ]),
     );
   }
 }
